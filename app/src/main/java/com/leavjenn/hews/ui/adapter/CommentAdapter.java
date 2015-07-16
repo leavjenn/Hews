01e8827032bdb1b6d2309eb3a3439ff90ca8e679
leavjenn <leavjenn@gmail.com>
2015-07-16 20:53:54 +0800

    fix (import): miss import

--- app/src/main/java/com/leavjenn/hews/ui/adapter/CommentAdapter.java
+++ app/src/main/java/com/leavjenn/hews/ui/adapter/CommentAdapter.java
@@ -12,6 +12,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
