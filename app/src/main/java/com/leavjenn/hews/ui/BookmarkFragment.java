57b2c9d47a8679ad691670d60fe98ff9a066b50b
leavjenn <leavjenn@gmail.com>
2015-11-29 21:53:49 +0800

    feat (bookmark): change name to bookmark for more proper naming, add bookmark option to CommentActivity menu

---
+++ app/src/main/java/com/leavjenn/hews/ui/BookmarkFragment.java
@@ -0,0 +1,57 @@
+package com.leavjenn.hews.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.leavjenn.hews.R;
+
+public class BookmarkFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public BookmarkFragment() {
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BookmarkFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BookmarkFragment newInstance(String param1, String param2) {
+        BookmarkFragment fragment = new BookmarkFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bookmark, container, false);
+    }
+
+}
--- app/src/main/java/com/leavjenn/hews/ui/CommentsActivity.java
+++ app/src/main/java/com/leavjenn/hews/ui/CommentsActivity.java
@@ -232,6 +232,10 @@ public class CommentsActivity extends AppCompatActivity implements
                 startActivity(Intent.createChooser(sendIntent, getString(R.string.share_link_to)));
                 break;
 
+            case R.id.action_bookmark:
+
+                break;
+
             case R.id.action_display:
                 if (!mWindow.isWindowShowing()) {
                     mWindow.show();
