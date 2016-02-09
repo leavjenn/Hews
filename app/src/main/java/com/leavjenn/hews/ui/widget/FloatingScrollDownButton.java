a3d8372a4f589e0fa7a54e8db3102b49fcfb8cfd
leavjenn <leavjenn@gmail.com>
2016-02-09 11:48:03 +0800

    refactor (FloatingScrollDownButton icon size): shrink FloatingScrollDownButton icon size.

--- app/src/main/java/com/leavjenn/hews/ui/widget/FloatingScrollDownButton.java
+++ app/src/main/java/com/leavjenn/hews/ui/widget/FloatingScrollDownButton.java
@@ -33,7 +33,7 @@ public class FloatingScrollDownButton extends android.support.design.widget.Floa
     public FloatingScrollDownButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         // override fab icon size
-        FloatingScrollDownButton.this.setPadding(8, 8, 8, 8);
+        FloatingScrollDownButton.this.setPadding(16, 16, 16, 16);
     }
 
     private Runnable scrollDownThread = new Runnable() {
