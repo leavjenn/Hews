40500537ea2a38ea7ee76be8fa8dbe8e55ec4ec3
chaosphinx <chaosphinx@gmail.com>
2022-08-06 00:49:49 +0800

    chore (update libs): update gradle and necessary libs. Remove Crashlytics.

--- app/src/main/java/com/leavjenn/hews/App.java
+++ app/src/main/java/com/leavjenn/hews/App.java
@@ -2,18 +2,9 @@ package com.leavjenn.hews;
 
 import android.app.Application;
 
-import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.core.CrashlyticsCore;
-
-import io.fabric.sdk.android.Fabric;
-
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        Fabric.with(this, new Crashlytics());
-        // disable Crashlytics while debugging
-        CrashlyticsCore core = new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build();
-        Fabric.with(this, new Crashlytics.Builder().core(core).build());
     }
 }
